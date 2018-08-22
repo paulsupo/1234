b4c36138d151229a58475d27c016e0a758b0760d
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:38:02 -0500

    clase

--- Juego/main.cpp
+++ Juego/main.cpp
@@ -4,12 +4,28 @@ using namespace std;
 
 int main()
 {
-    int a;
-    int b;
-    int z;
+    /*int a;
+    float b;
+    float z;
+    int x;
+    int y;
     cin >> a;
     cin >> b;
-    z = a + b;
-    cout << "La suma de "<< a <<" y "<< b << " es igual a " << z << endl;
+    x = a + b;
+    y = a * b;
+    z = a / b;
+
+    cout <<"\nLa suma de "<< a <<" y "<< b << " es igual a " << x <<endl;
+    cout <<"\nLa multiplicacion de "<< a << " y "<< b <<" es igual a "<< y<< endl;
+    cout <<"\nLa division de "<< a << " y " << b << " es igual a "<< z<< endl;
+    */
+    short int a;
+    short int z;
+    a=32766;
+    z=a+1;
+    cout <<z<<endl;
+    z=z+1;
+    cout <<z<<endl;
+    return 0;
     return 0;
 }
